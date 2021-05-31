c094ef4fda74c895da351edb7bed5d31661a7c0c
sebinn09 <sebinn09@gmail.com>
2021-05-31 15:05:38 +0530

    initial commit of maven_demo_01

---
+++ src/test/java/starting_project_01/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package starting_project_01.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
